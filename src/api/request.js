033d1f61e39bad1d482c24a0d05392ebc9d0ec75
bamlubi-lab <bamlubi-lab@ustc.edu.cn>
2022-10-18 20:08:11 +0800

    feat: split mode of "dev" & "prod"

--- src/api/request.js
+++ src/api/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // 创建一个自定义的Axios对象
 const Axios = axios.create({
-  baseURL: "http://127.0.0.1:8888",
+  baseURL: import.meta.env.VITE_APP_GO_SERVICE_URL,
   timeout: 3000,
   /*也可以不设置Content-Type，影响是在你发送请求时
     Vue会先发送OPTIONS包探测路由是否存在，需要后端也做设置响应OPTIONS
